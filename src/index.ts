cf785119e2b100421c691159b5cbe097f95a332d
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-06-08 10:01:05 +0530

    idex.ts file added

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Calculator from "./calculator";
+const calculator = new Calculator();
+console.log(calculator.add(2,3));
+console.log(calculator.sub(2,3));
+console.log(calculator.multiplication(2,3));
+console.log(calculator.division(2,3));
+
