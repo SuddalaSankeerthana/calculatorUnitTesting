179699ac148224d2663c764d604baf9c7836e393
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-06-10 10:47:55 +0530

    used prettier for formating

--- src/calculator.spec.ts
+++ src/calculator.spec.ts
@@ -1,28 +1,28 @@
-import Calculator from "./calculator";
+import Calculator from './calculator';
 describe('test calculator', () => {
-    test('addition', () => {
-      const calculator = new Calculator();
-      const result = calculator.add(2, 3);
-      expect(result).toBe(5);
-    });
-    test('subtraction', () => {
-      const calculator = new Calculator();
-      const result = calculator.sub(2, 3);
-      expect(result).toBe(-1);
-    });
-    test('multiplication', () => {
-      const calculator = new Calculator();
-      const result = calculator.multiplication(2, 3);
-      expect(result).toBe(6);
-    });
-    test('divison with normal case', () => {
-      const calculator = new Calculator();
-      const result = calculator.division(10, 5);
-      expect(result).toBe(2);
-    });
-    test('division with throwing error case', () => {
-      const calculator = new Calculator();
-  
-      expect(()=>calculator.division(10, 0)).toThrow('Zero Division Error');
-    });
+  test('addition', () => {
+    const calculator = new Calculator();
+    const result = calculator.add(2, 3);
+    expect(result).toBe(5);
   });
+  test('subtraction', () => {
+    const calculator = new Calculator();
+    const result = calculator.sub(2, 3);
+    expect(result).toBe(-1);
+  });
+  test('multiplication', () => {
+    const calculator = new Calculator();
+    const result = calculator.multiplication(2, 3);
+    expect(result).toBe(6);
+  });
+  test('divison with normal case', () => {
+    const calculator = new Calculator();
+    const result = calculator.division(10, 5);
+    expect(result).toBe(2);
+  });
+  test('division with throwing error case', () => {
+    const calculator = new Calculator();
+
+    expect(() => calculator.division(10, 0)).toThrow('Zero Division Error');
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import Calculator from "./calculator";
+import Calculator from './calculator';
 const calculator = new Calculator();
-console.log(calculator.add(2,3));
-console.log(calculator.sub(2,3));
-console.log(calculator.multiplication(2,3));
-console.log(calculator.division(2,3));
-
+console.log(calculator.add(2, 3));
+console.log(calculator.sub(2, 3));
+console.log(calculator.multiplication(2, 3));
+console.log(calculator.division(2, 3));
